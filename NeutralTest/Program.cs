f717efed53339ba2fc1502296f37bc91d13541d7
Sebastian Drela <sebastian.drela@wp.pl>
2025-04-21 22:20:31 +0200

    increase epochs

--- NeutralTest/Program.cs
+++ NeutralTest/Program.cs
@@ -10,7 +10,7 @@ internal class Program
     {
         var network = new NeuralNetworkBuilder()
             .WithArchitecture(8, 12, 12, 8)
-            .WithEpochs(5000)
+            .WithEpochs(200 * 100)
             .WithBatchSize(64)
             .WithLearningRate(0.01f)
             .WithWeightDecay(1e-5f)
