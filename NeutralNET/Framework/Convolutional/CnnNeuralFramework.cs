779baf291f6301e6687770104471e8dccc1ca327
DESKTOP-VE3UNO0\Sebastian <sebastian.drela@wp.pl>
2026-09-20 03:35:57 +0200

    add back comment

--- NeutralNET/Framework/Convolutional/CnnNeuralFramework.cs
+++ NeutralNET/Framework/Convolutional/CnnNeuralFramework.cs
@@ -10,6 +10,12 @@ using static NeutralNET.Activation.ActivationSelector;
 
 namespace NeutralNET.Framework.Neural.CNN;
 
+
+/// <summary>
+/// Zero‑GC CNN framework with full object and buffer pooling, pluggable optimizers,
+/// and low-latency P/Invoke CUDA/cuBLAS GPU matrix acceleration.
+/// </summary>
+///
 public sealed unsafe class CnnNeuralFramework
 {
     private const int Avx256Size = 8;
